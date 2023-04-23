9ded923de645de13d1c45b03984d39225e9d4636
tim_curchod <timofeyc@hotmail.com>
2023-04-23 12:08:38 +1000

    created split register login function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,8 @@ import { MongoRepository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 @Injectable()
 export class UsersService {
@@ -12,8 +14,33 @@ export class UsersService {
     private usersRepository: MongoRepository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.usersRepository.save(createUserDto);
+  async create(createUserDto: any) {
+    if (createUserDto.name) {
+      console.log('register new user', createUserDto);
+      const salt = await bcrypt.genSalt(10)
+      createUserDto.password = await bcrypt.hash(createUserDto.password, salt)
+      return this.usersRepository.save(createUserDto);
+    } else {
+      const salt = await bcrypt.genSalt(10)
+      if (createUserDto.id) {
+        console.log('found login for', createUserDto)
+        const hashedPassword = await bcrypt.hash(createUserDto.password, salt)
+        const user = await this.usersRepository.findOne(createUserDto.id);
+        const match = await bcrypt.compare(user.password, hashedPassword)
+        if (user && match) {
+          console.log('pw match')
+          const token = jwt.sign(createUserDto.id, process.env.JWT_SECRET, { expiresIn: '12h' })
+          const response = {
+            _id: createUserDto.id,
+            firstName: createUserDto.name,
+            email: user.email,
+            userToken: token,
+          }
+        createUserDto.password = await bcrypt.hash(createUserDto.password, salt)
+        return response;
+        }
+      }
+    }
   }
 
   findAll() {
