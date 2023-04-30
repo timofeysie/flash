8218e3f232ce48971a876eb4f5d7854ca5f62046
tim_curchod <timofeyc@hotmail.com>
2023-04-30 13:10:10 +1000

    added bcrypt to the package file

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
-    origin: false,
-    preflightContinue: true,
-    methods: ['GET,POST,OPTIONS,DELETE,PUT'],
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
