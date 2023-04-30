e4fc868c2b71b3d98a6a1c2b3db448c5ab7d15ef
tim_curchod <timofeyc@hotmail.com>
2023-04-30 12:56:32 +1000

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: false,
+    preflightContinue: true,
+    methods: ['GET,POST,OPTIONS,DELETE,PUT'],
+  });
   await app.listen(3000);
 }
 bootstrap();
