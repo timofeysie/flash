90df6c27cad8c859a4fb91e191f65c4981142ae6
tim_curchod <timofeyc@hotmail.com>
2023-04-30 12:36:02 +1000

    changed the bcrypt import style

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,7 +4,7 @@ import { MongoRepository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 @Injectable()
